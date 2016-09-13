44a6fe34cd8fb4a5ad7a7ee583bc8ce3f3031a93
Liczek <Pawel@Pawels-Mac.local>
2016-09-13 20:16:49 +0200

    add project

---
+++ TVCandXIB/TVCandXIB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TVCandXIB
+//
+//  Created by Pawel on 13.09.2016.
+//  Copyright © 2016 LiczekCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
